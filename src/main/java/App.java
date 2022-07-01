d01ee4979152aae2b1a1deaa27e8a9ecf2ca396a
Mark <i.love.rock182@gmail.com>
2022-07-01 13:26:14 +0300

    Add pojo class Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        System.out.println(applicationContext.getBean("helloworld").equals(applicationContext.getBean("helloworld")));
+        System.out.println( applicationContext.getBean("cat").equals( applicationContext.getBean("cat")));
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+    private String name;
+
+    Cat(){
+
+    }
+
+    Cat(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
